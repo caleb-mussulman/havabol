9d07bf7eec8ebcd8b7c17aff106984c8490c4249
Taylor <tcbrauer@gmail.com>
2017-02-17 17:58:00 -0600

    Initialized constructor for STEntry.

--- src/havabol/STEntry.java
+++ src/havabol/STEntry.java
@@ -12,15 +12,19 @@ package havabol;
 class STEntry
 {
 
+	String symbol;
+	int primClassif;
+	
 	public static final int OR = 1;
     public static final int AND = 2;
     public static final int NOT = 3;
     public static final int IN = 4;
     public static final int NOTIN = 5;
 	
-    public STEntry(String symbol, int primClassif)
+    public STEntry(String aSymbol, int aPrimClassif)
     {
-        
+        this.symbol = aSymbol;
+        this.primClassif = aPrimClassif;
     }
     
 }
