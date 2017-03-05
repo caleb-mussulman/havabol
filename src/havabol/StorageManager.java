8814eb978e69fa257e56d8b1e1d771ebe4be34be
stevencenci <s.cenci05@gmail.com>
2017-03-04 21:38:12 -0600

    Began to add functionality of the StorageManager. SM uses a HashMap<String, String>

--- src/havabol/StorageManager.java
+++ src/havabol/StorageManager.java
@@ -1,5 +1,6 @@
 package havabol;
 
+import java.util.HashMap;
 /**
  * 
  * <p>
@@ -8,9 +9,49 @@ package havabol;
 public class StorageManager
 {
     
-    StorageManager(String symbol)
+    String symbol;
+    ResultValue value;
+    
+    public HashMap<String,ResultValue> sm;      //Declare the hashmap
+    
+    /**
+     * Simply creates HashMap that will effectively be our
+     * StorageManager.
+     * <p>
+     */
+    StorageManager()
     {
+        //Create a new HashMap -- Empty
+        sm = new HashMap<String,ResultValue>();
+    }
+    
+    /**
+     * The function that will be used to store a variable value
+     * into the HashMap using it's associated symbol (variable)
+     * <p>
+     * 
+     * @param symbol
+     * @param value 
+     */
+    void storeVariableValue(String symbol, String value){
         
+    }
+    
+    /**
+     * The function that will be used to get the value of the passed
+     * symbol (variable) from the prase(?) to retrieve the value 
+     * of the symbol(variable).
+     * <p>
+     * Quickly does a hash lookup for the passed String symbol and returns
+     * the value as a string...
+     * Conversion will be done by numeric class
+     * @param symbol
+     * @return ResultValue
+     */
+    String getVariableValue(String symbol){
+        String value;
+        value = get.sm(symbol);    
+        return value;
     }
     
 }
